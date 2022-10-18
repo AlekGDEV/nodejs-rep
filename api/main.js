e95ff8d644f1b204daf50c78934c94ccd6743a83
AlekGDEV <AlexandreGDEV@outlook.com>
2022-10-18 10:37:51 -0300

    add server

--- api/main.js
+++ api/main.js
@@ -1,5 +1,21 @@
+
+
+const http = require('http');
+
 const produto = require('./acoes/produto');
 const cliente = require('./acoes/client');
+
+const host = 'localhost';
+const port = '12050'
+
+//req = requisição.
+//res = resposta.
+
+function execucao(req, res){
+    resposta.end('Teste');
+}
+
+const server = http.createServer(execucao)
 
 console.log(produto.listar());
 console.log(cliente.listar());
