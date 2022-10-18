3d7792b59d7a984aae5c0b6935cf5d59960ac548
AlekGDEV <AlexandreGDEV@outlook.com>
2022-10-18 10:40:54 -0300

    comentando codigo

--- api/main.js
+++ api/main.js
@@ -1,5 +1,3 @@
-
-
 const http = require('http');
 
 const produto = require('./acoes/produto');
@@ -11,11 +9,13 @@ const port = '12050'
 //req = requisição.
 //res = resposta.
 
+//função que será executada sempre que houver uma requisição por parte do cliente
 function execucao(req, res){
     resposta.end('Teste');
 }
 
+//criando o servidor e definindo a função que será executada
 const server = http.createServer(execucao)
 
-console.log(produto.listar());
-console.log(cliente.listar());
+//subindo o servidor pra ficar escutando as requisiçoes
+server.listen(port, host, () => {})
