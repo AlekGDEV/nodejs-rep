31a17da1c27308708a67336eba1e68aedbeb7d9e
AlekGDEV <AlexandreGDEV@outlook.com>
2022-10-18 10:11:50 -0300

    add pasta api

---
+++ api/main.js
@@ -0,0 +1,7 @@
+const perfil = require('./acoes/perfil');
+const cadastrar = require('./acoes/cadastro');
+const listar = require('./acoes/listar');
+
+console.log(perfil());
+console.log(cadastrar());
+console.log(listar());
